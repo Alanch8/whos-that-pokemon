516c3f09171a487e543d3f4d9a8c669cf788abb5
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-09-19 18:07:16 +0200

    added uppercase to the first letter of the pokemon names

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,78 +1,66 @@
 <template>
-    <h1 v-if="!pokemon">Please wait...</h1>
+  <h1 v-if="!pokemon">Please wait...</h1>
 
-    <div v-else>
-        <h1>Who's that Pokémon?</h1>
-        
-        <PokemonPicture 
-            :pokemon-id="pokemon.id" 
-            :show-pokemon="showPokemon" 
-        />
+  <div v-else>
+    <h1>Who's that Pokémon?</h1>
 
-        <PokemonOptions 
-            :pokemons="pokemonArr"
-            @selection-pokemon="checkAnswer"
-        />
+    <PokemonPicture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
 
-        <template v-if="showAnswer">
-            <h2 class="fade-in">{{ message }}</h2>
-            <button @click="newGame">
-                Next
-            </button>
-        </template>
+    <PokemonOptions :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
 
-    </div>
-    
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">Next</button>
+    </template>
+  </div>
 </template>
 
 <script>
-import PokemonOptions from '@/components/PokemonOptions'
-import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from "@/components/PokemonOptions";
+import PokemonPicture from "@/components/PokemonPicture";
 
-import getPokemonOptions from '@/helpers/getPokemonOptions'
-
+import getPokemonOptions from "@/helpers/getPokemonOptions";
 
 export default {
-    components: { PokemonOptions, PokemonPicture },
-    data() {
-        return {
-            pokemonArr: [],
-            pokemon: null,
-            showPokemon: false,
-            showAnswer: false,
-            message: ''
-        }
+  components: { PokemonOptions, PokemonPicture },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      message: "",
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions();
+
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
     },
-    methods: {
-        async mixPokemonArray() {
-            this.pokemonArr = await getPokemonOptions()
+    checkAnswer(selectedId) {
+      this.showPokemon = true;
+      this.showAnswer = true;
+      let pokemonName = this.pokemon.name;
+      pokemonName = pokemonName[0].toUpperCase() + pokemonName.slice(1);
 
-            const rndInt = Math.floor( Math.random() * 4 )
-            this.pokemon = this.pokemonArr[ rndInt ]
-        },
-        checkAnswer( selectedId ) {
-            this.showPokemon = true
-            this.showAnswer  = true
-
-            if( selectedId === this.pokemon.id ) {
-                this.message = `Right, ${ this.pokemon.name }!`
-            } else {
-                this.message = `Oops, it was ${ this.pokemon.name }.`
-            }
-        },
-        newGame() {
-
-            this.showPokemon = false
-            this.showAnswer  = false
-            this.pokemonArr  = []
-            this.pokemon     = null
-            this.mixPokemonArray()            
-
-        }
+      if (selectedId === this.pokemon.id) {
+        this.message = `Right, ${pokemonName}!`;
+      } else {
+        this.message = `Oops, it was ${pokemonName}.`;
+      }
     },
-    mounted() {
-        this.mixPokemonArray()
-    }
-
-}
-</script>
+    newGame() {
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.pokemonArr = [];
+      this.pokemon = null;
+      this.mixPokemonArray();
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
+};
+</script>   
